bb54a164d090a69e6910fa1448a5a73300db0757
mattia biscontini <mattiabiscontini@gmail.com>
2021-06-18 13:45:39 +0100

    first commit

---
+++ day5/day5.js
@@ -0,0 +1,35 @@
+let options = ["Water", "Wine", "Coffee", "Juice", "Beer"];
+let price = [0.50, 8, 0.80, 1.50, 4.50];
+
+var result = document.getElementById("result");
+let button = document.getElementById("button");
+
+
+button.addEventListener("click", () => {
+    var balance = document.getElementById("input").value;
+    var choice = document.getElementById("input2").value;
+   
+
+
+
+    if (choice == options[0] && balance >= price[0] || 
+        choice == options[1] && balance >= price[1] ||
+        choice == options[2] && balance >= price[2] ||
+        choice == options[3] && balance >= price[3] ||
+        choice == options[4] && balance >= price[4]){
+        result.textContent = "enjoy your drink!"
+
+    } else {
+        result.textContent = "not enough money!"
+
+    }
+})
+
+
+
+
+
+
+
+
+
